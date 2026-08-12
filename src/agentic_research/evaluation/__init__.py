e531c66e06a85520eeb5814db047ca15436730be
ZHX4 <120254893+ZHX4@users.noreply.github.com>
2026-08-12 23:41:08 +0100

    Add Phase 8 evaluation package

---
+++ src/agentic_research/evaluation/__init__.py
@@ -0,0 +1,17 @@
+"""Phase 8 evaluation toolkit."""
+
+from .comparison import compare_baselines, evaluate_ablation, summarize_costs
+from .engine import evaluate_extraction, evaluate_labels, evaluate_retrieval, evaluate_temporal, stable_run_id
+from .human import evaluate_human_ratings
+
+__all__ = [
+    "compare_baselines",
+    "evaluate_ablation",
+    "evaluate_extraction",
+    "evaluate_human_ratings",
+    "evaluate_labels",
+    "evaluate_retrieval",
+    "evaluate_temporal",
+    "stable_run_id",
+    "summarize_costs",
+]
